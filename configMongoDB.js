a00cc98b94beac9b4cec26f1d82f25b8844c5702
gustavodealmeida <guttoalmeida@hotmail.com>
2019-06-13 13:24:20 -0300

    Atualizando Informação do db

--- configMongoDB.js
+++ configMongoDB.js
@@ -1,6 +1,6 @@
 module.exports = {
-    uri: 'mongodb://localhost:27017/mydb',
-    db: 'mydb',
+    uri: 'mongodb+srv://ec38b:<scrYBK8DWUdSE961>@cluster0-fx2wx.mongodb.net/test?retryWrites=true&w=majority',
+    db: 'ec38b',
     options: {
         useNewUrlParser: true
     }
