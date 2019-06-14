09ee9d0468201bbbd22705e6ebc0c54b8368e9f8
pradoplucas <lucaspinto@alunos.utfpr.edu.br>
2019-06-14 04:16:13 -0300

    Final, eu acho, depois de excluir a pasta do mongo sem querer

---
+++ configMongoDB.js
@@ -0,0 +1,8 @@
+    
+module.exports = {
+    uri: 'mongodb://test:123456a@ds337377.mlab.com:37377/heroku_d3dj255t',
+    db: 'heroku_d3dj255t',
+    options: {
+        useNewUrlParser: true
+    }
+};
